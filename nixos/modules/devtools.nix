fdd9b43a4f30c6c3474676484e3f9db105341135
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2025-10-31 19:33:32 +0100

    Create devtools.nix

---
+++ nixos/modules/devtools.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    #Dev enviornment
+    neovim
+    unzip
+    fd
+    fastfetch
+    tree
+    htop
+    btop
+    ripgrep
+    jq
+
+
+
+    #Lanuage tools
+    gcc
+    gnumake
+    cmake
+    pkg-config
+    python3
+    nodejs_20
+    yarn
+    go
+    rustup
+
+
+    #Shell utils
+    zsh
+    oh-my-zsh
+    starship
+    bat
+    fzf
+
+
+    #Hacking tools
+    ghidra
+    gdb
+    capstone
+    wireshark
+    tcpdump
+    nmap
+    netcat
+    aircrack-ng
+    iw
+    john
+    hashcat
+    metasploit
+    exploitdb
+    sleuthkit
+    autopsy
+    volatility3
+
+
+    #osint
+    theharvester
+    snscrape
+    exiftool
+    mapscii
+
+  ];
+}
